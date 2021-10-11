7877738f4b3111b97e1cee3d07bd6e1caf1b0a9c
Edgar <edgargonzalez.info@gmail.com>
2021-10-11 22:17:37 +0100

    Initial Setup

---
+++ FabulousPlayGround.iOS/AppDelegate.fs
@@ -0,0 +1,25 @@
+﻿// Copyright 2018 Fabulous contributors. See LICENSE.md for license.
+namespace FabulousPlayGround.iOS
+
+open System
+open UIKit
+open Foundation
+open Xamarin.Forms
+open Xamarin.Forms.Platform.iOS
+
+[<Register ("AppDelegate")>]
+type AppDelegate () =
+    inherit FormsApplicationDelegate ()
+
+    override this.FinishedLaunching (app, options) =
+        Forms.Init()
+        let appCore = new FabulousPlayGround.App()
+        this.LoadApplication (appCore)
+        base.FinishedLaunching(app, options)
+
+module Main =
+    [<EntryPoint>]
+    let main args =
+        UIApplication.Main(args, null, nameof(AppDelegate))
+        0
+
